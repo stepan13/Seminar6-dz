102afd21a484e0a09478997953d01fceb17db305
Vanger <stepan13@gmail.com>
2022-12-02 12:37:33 +0000

    inintial

---
+++ 41.countPositiveInput/Program.cs
@@ -0,0 +1,20 @@
+﻿int input = -1;
+int i = 0;
+int count = 0;
+while (input != 0)
+{
+    i++;
+    input = AskIntNumber($"Введите {i} число или 0 для выхода:");
+    if (input == 0)
+        break;
+    else if (input > 0)
+        count++;
+}
+Console.WriteLine($"Введено {i} чисел. Из них положительных: {count}");
+
+int AskIntNumber(string WelcomeText)
+{
+    Console.Write(WelcomeText);
+    string input = Console.ReadLine() ?? string.Empty;
+    return Convert.ToInt32(input);
+}
---
+++ 43.lineCross/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double k1 = AskDoubleNumber("Введите k1: ");
+double b1 = AskDoubleNumber("Введите b1: ");
+double k2 = AskDoubleNumber("Введите k2: ");
+double b2 = AskDoubleNumber("Введите b2: ");
+
+double[] crossCoordinates = FindCrossPoint(k1,b1,k2,b2);
+PrintArray(crossCoordinates);
+
+double[] FindCrossPoint(double k1, double b1, double k2, double b2)
+{
+    double[] result = new double[2];
+    result[0] = (b2-b1)/(k1-k2);
+    result[1] = (k1*b2-k2*b1)/(k1-k2);
+
+    return result;
+}
+
+double AskDoubleNumber(string WelcomeText)
+{
+    Console.Write(WelcomeText);
+    string input = Console.ReadLine() ?? string.Empty;
+    return Convert.ToDouble(input);
+}
+
+void PrintArray(double[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+        Console.Write(array[i] + ((i == count - 1) ? "]" : ", "));
+
+}
